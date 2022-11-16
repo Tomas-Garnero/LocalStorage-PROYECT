f9105f92f38808ec705269a8f94b05c1ef5f0284
unknown <tomygarnero9429@gmail.com>
2022-11-16 17:43:13 -0300

    Added function to add tweets

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Variables
 const formulario = document.querySelector("#formulario");
 const listaTweets = document.querySelector("#lista-tweets");
-const tweets = [];
+let tweets = [];
 
 
 // Event Listeners
@@ -14,4 +14,68 @@ function eventListeners() {
 // Funciones
 function agregarTweets(e) {
     e.preventDefault();
+    
+    // Textarea donde escribe el usuario
+    const tweet = document.querySelector("#tweet").value;
+
+    // Validacion
+    if (tweet === "") {
+        mostrarError("Un mensaje no puede ir vacio");
+        return;  // evita que se ejecuten mas lineas de codigo
+    }
+
+    // Añadir al arreglo de tweets
+    const tweetObj = {
+        id: Date.now,
+        tweet
+    }
+
+    tweets = [...tweets, tweetObj];
+    
+    // Una vez agregado creamos el HTML
+    crearHTML();
+
+    // Reiniciar el formulario
+    formulario.reset();
+}
+
+// Mostrar mensaje de error
+    function mostrarError(error) {
+        const mensajeError = document.createElement("p");
+        mensajeError.textContent = error;
+        mensajeError.classList.add("error");
+
+        // Insertarlo en el contenido
+        const contenido = document.querySelector("#contenido");
+        contenido.appendChild(mensajeError);
+
+        // Elimina la alerta despues de 3 segundos
+        setTimeout(() => {
+            mensajeError.remove();
+        }, 3000);
+}
+
+// Muestra un listado de tweets
+function crearHTML() {
+
+    limpiarHTML();
+
+    if (tweets.length > 0) {
+        tweets.forEach(tweet => {
+
+            // Crear el HTML
+            const li = document.createElement("li");
+            li.innerText = tweet.tweet;
+
+            // Insertarlo en el HTML
+            listaTweets.appendChild(li);
+        })
+    }
+}
+
+// Limpiar el HTML
+function limpiarHTML() {
+    while(listaTweets.firstChild) {
+        listaTweets.removeChild(listaTweets.firstChild);
+    }
 }
